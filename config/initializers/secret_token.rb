8d4b263e2647f8e47aab9153593d5d42ff8ecf84
David Segalabutla <david.segalabutla@nlsa.ac.za>
2012-02-26 14:29:01 +0200

    sdasd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sundayapp::Application.config.secret_token = '3946dab3bcb99034266cbf004dcb77c37db42d560b1802eb00125fb345e3e2ee556af0a51520adf1d8485ebf4a7737f0f7c02f06fb0c0c61e33e40c205ca3e92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sundayapp::Application.config.session_store :cookie_store, :key => '_sundayapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sundayapp::Application.config.session_store :active_record_store
